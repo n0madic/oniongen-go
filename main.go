b2b18c0dde5b5d73a13a286b026b794ce3161120
Alexander Sheiko <alexander.sheiko@mgid.com>
2024-09-25 17:19:40 +0300

    Refactor key generation and address checking logic

--- main.go
+++ main.go
@@ -25,8 +25,8 @@ var (
 
 const batchSize = 1000 // Batch size for key generation
 
-// generateBatch generates a batch of keys and checks them against the regular expression
-func generateBatch(wg *sync.WaitGroup, re *regexp.Regexp, resultChan chan<- string) {
+// generateBatch generates a batch of keys and checks them against the regular expressions
+func generateBatch(wg *sync.WaitGroup, regexps []*regexp.Regexp, resultChan chan<- string, saveWg *sync.WaitGroup) {
 	var buffer bytes.Buffer
 	buffer.Grow(100) // Pre-allocate memory for the buffer
 	batch := make([]ed25519.PublicKey, batchSize)
@@ -45,11 +45,15 @@ func generateBatch(wg *sync.WaitGroup, re *regexp.Regexp, resultChan chan<- stri
 		for i, publicKey := range batch {
 			buffer.Reset()
 			onionAddress := encodePublicKey(publicKey, &buffer)
-			if re.MatchString(onionAddress) {
-				resultChan <- onionAddress
-				go save(onionAddress, publicKey, expandSecretKey(batchSecretKeys[i])) // Asynchronous saving
-				atomic.AddInt64(&found, 1)
-				wg.Done()
+			for _, re := range regexps {
+				if re.MatchString(onionAddress) {
+					resultChan <- onionAddress
+					saveWg.Add(1)
+					go save(onionAddress, publicKey, expandSecretKey(batchSecretKeys[i]), saveWg) // Asynchronous saving
+					atomic.AddInt64(&found, 1)
+					wg.Done()
+					break
+				}
 			}
 		}
 	}
@@ -78,13 +82,30 @@ func encodePublicKey(publicKey ed25519.PublicKey, buffer *bytes.Buffer) string {
 }
 
 // save stores the generated keys and address in files
-func save(onionAddress string, publicKey ed25519.PublicKey, secretKey [64]byte) {
-	os.MkdirAll(onionAddress, 0700)
+func save(onionAddress string, publicKey ed25519.PublicKey, secretKey [64]byte, wg *sync.WaitGroup) {
+	defer wg.Done()
+	err := os.MkdirAll(onionAddress, 0700)
+	if err != nil {
+		fmt.Printf("Error creating directory %s: %v\n", onionAddress, err)
+		return
+	}
 	secretKeyFile := append([]byte("== ed25519v1-secret: type0 ==\x00\x00\x00"), secretKey[:]...)
-	os.WriteFile(onionAddress+"/hs_ed25519_secret_key", secretKeyFile, 0600)
+	err = os.WriteFile(onionAddress+"/hs_ed25519_secret_key", secretKeyFile, 0600)
+	if err != nil {
+		fmt.Printf("Error writing secret key file for %s: %v\n", onionAddress, err)
+		return
+	}
 	publicKeyFile := append([]byte("== ed25519v1-public: type0 ==\x00\x00\x00"), publicKey...)
-	os.WriteFile(onionAddress+"/hs_ed25519_public_key", publicKeyFile, 0600)
-	os.WriteFile(onionAddress+"/hostname", []byte(onionAddress+".onion\n"), 0600)
+	err = os.WriteFile(onionAddress+"/hs_ed25519_public_key", publicKeyFile, 0600)
+	if err != nil {
+		fmt.Printf("Error writing public key file for %s: %v\n", onionAddress, err)
+		return
+	}
+	err = os.WriteFile(onionAddress+"/hostname", []byte(onionAddress+".onion\n"), 0600)
+	if err != nil {
+		fmt.Printf("Error writing hostname file for %s: %v\n", onionAddress, err)
+		return
+	}
 }
 
 // printStats outputs generation statistics every 5 seconds
@@ -101,26 +122,42 @@ func printStats(startTime time.Time) {
 
 func main() {
 	// Check command-line arguments
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: program <regex> <num_addresses>")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: program <regex1> [<regex2> ...] [<num_addresses>]")
 		os.Exit(1)
 	}
 
-	// Compile the regular expression
-	re, err := regexp.Compile(os.Args[1])
-	if err != nil {
-		fmt.Println("Invalid regex:", err)
-		os.Exit(1)
+	// Parse and compile regular expressions
+	var regexps []*regexp.Regexp
+	numAddresses := 1
+	var err error
+
+	for i := 1; i < len(os.Args); i++ {
+		if i == len(os.Args)-1 {
+			// Check if the last argument is a number
+			numAddresses, err = strconv.Atoi(os.Args[i])
+			if err == nil {
+				break
+			}
+		}
+		re, err := regexp.Compile(os.Args[i])
+		if err != nil {
+			fmt.Printf("Invalid regex '%s': %v\n", os.Args[i], err)
+			os.Exit(1)
+		}
+		regexps = append(regexps, re)
 	}
 
-	// Parse the number of addresses
-	numAddresses, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		fmt.Println("Invalid number of addresses:", err)
+	if len(regexps) == 0 {
+		fmt.Println("At least one valid regex is required")
 		os.Exit(1)
 	}
 
 	var wg sync.WaitGroup
+	var saveWg sync.WaitGroup
+	if numAddresses < 1 {
+		numAddresses = 1
+	}
 	wg.Add(numAddresses)
 	resultChan := make(chan string, numAddresses)
 
@@ -129,7 +166,7 @@ func main() {
 
 	// Start goroutines for address generation
 	for i := 0; i < runtime.NumCPU(); i++ {
-		go generateBatch(&wg, re, resultChan)
+		go generateBatch(&wg, regexps, resultChan, &saveWg)
 	}
 
 	// Goroutine to print found addresses
@@ -141,4 +178,7 @@ func main() {
 
 	wg.Wait()
 	close(resultChan)
+
+	// Wait for all save operations to complete
+	saveWg.Wait()
 }
